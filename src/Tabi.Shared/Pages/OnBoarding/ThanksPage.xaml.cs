8cfdd410decf5f9097434fe781bb850fad7c844a
Unknown <vpl@vpl.me>
2018-05-02 19:20:58 +0200

    XAMCompilation turned on for a few pages, updated LinkDescription

--- src/Tabi.Shared/Pages/OnBoarding/ThanksPage.xaml.cs
+++ src/Tabi.Shared/Pages/OnBoarding/ThanksPage.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using Autofac;
 using Tabi.Shared.ViewModels;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Tabi.Shared.Pages.OnBoarding
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ThanksPage : ContentPage
     {
         ThanksViewModel ViewModel => vm ?? (vm = BindingContext as ThanksViewModel);
--- src/Tabi.Shared/Pages/OnBoarding/WelcomePage.xaml.cs
+++ src/Tabi.Shared/Pages/OnBoarding/WelcomePage.xaml.cs
@@ -5,9 +5,11 @@ using System.Windows.Input;
 using Autofac;
 using Tabi.Shared.ViewModels;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Tabi.Shared.Pages.OnBoarding
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WelcomePage : ContentPage
     {
         WelcomeViewModel ViewModel => vm ?? (vm = BindingContext as WelcomeViewModel);
