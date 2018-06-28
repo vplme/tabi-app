5b50b2cf4c46936bdc7c027001a028c8852c7b21
vpl <vpl@vpl.me>
2018-06-28 12:46:03 +0200

    Move motivedetailpage to one base

---
+++ src/Tabi.Shared/ViewModels/DetailMotiveViewModel.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MvvmHelpers;
+using Tabi.DataStorage;
+using Tabi.Shared.Pages;
+using Tabi.Shared.Resx;
+using Xamarin.Forms;
+
+namespace Tabi.Shared.ViewModels
+{
+    public abstract class DetailMotiveViewModel : BaseViewModel
+    {
+
+        protected readonly IRepoManager _repoManager; 
+        protected readonly TabiConfiguration _configuration;
+
+        protected readonly INavigation _navigation;
+        protected readonly MotiveSelectionViewModel _motiveSelectionViewModel;
+        protected MotiveOptionViewModel CustomMotiveOption = new MotiveOptionViewModel();
+
+        protected bool saved;
+
+        public DetailMotiveViewModel(IRepoManager repoManager,
+                                         INavigation navigation,
+                                         TabiConfiguration configuration,
+                                     MotiveSelectionViewModel motiveSelectionViewModel)
+        {
+            _navigation = navigation ?? throw new ArgumentNullException(nameof(navigation));
+            _motiveSelectionViewModel = motiveSelectionViewModel ?? throw new ArgumentNullException(nameof(motiveSelectionViewModel));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _repoManager = repoManager ?? throw new ArgumentNullException(nameof(repoManager));
+
+            SaveCommand = new Command(async () =>
+            {
+                SaveViewModel();
+                await _navigation.PopModalAsync();
+
+            });
+            CancelCommand = new Command(async () =>
+            {
+                ResetViewModel();
+                await PopPageAsync();
+            });
+
+            _motiveSelectionViewModel.PropertyChanged += _motiveSelectionViewModel_PropertyChanged;
+
+            PossibleMotives = new ObservableRangeCollection<MotiveOptionViewModel>();
+
+        
+        }
+
+        protected void SetupMotives()
+        {
+            // Setup possible motives
+            foreach (MotiveOption mo in _configuration.Motive.Options)
+            {
+                string translatedText = typeof(AppResources).GetProperty($"{mo.Id}MotiveText")?.GetValue(null) as string;
+                string text = translatedText ?? mo.Text;
+                MotiveOptionViewModel motive = new MotiveOptionViewModel() { Text = text, Id = mo.Id };
+                if (motive.Id == MotiveText)
+                {
+                    motive.Selected = true;
+                    _motiveSelectionViewModel.SelectedMotiveOption = motive;
+                }
+                PossibleMotives.Add(motive);
+            }
+
+            foreach (MotiveOption mo in _configuration.Motive.OtherOptions)
+            {
+                string translatedText = typeof(AppResources).GetProperty($"{mo.Id}MotiveText")?.GetValue(null) as string;
+                string text = translatedText ?? mo.Text;
+                if (mo.Id == MotiveText)
+                {
+                    CustomMotiveOption = new MotiveOptionViewModel() { Text = text, Id = mo.Id };
+                    _motiveSelectionViewModel.SelectedMotiveOption = CustomMotiveOption;
+                    CustomMotiveOption.Selected = true;
+                    _motiveSelectionViewModel.CustomMotive = true;
+
+                    break;
+                }
+            }
+        }
+
+        protected abstract string MotiveText { get; }
+        protected abstract void ResetViewModel();
+
+        protected abstract void SaveViewModel();
+
+        ObservableRangeCollection<MotiveOptionViewModel> possibleMotives;
+
+        public ObservableRangeCollection<MotiveOptionViewModel> PossibleMotives
+        {
+            get => possibleMotives;
+            set => SetProperty(ref possibleMotives, value);
+        }
+
+
+        private async Task PopPageAsync()
+        {
+            await _navigation.PopModalAsync();
+        }
+
+        public ICommand SaveCommand { get; set; }
+
+        public ICommand CancelCommand { get; set; }
+
+        public void OptionSelected(MotiveOptionViewModel motive)
+        {
+            if (motive.Id == "Other")
+            {
+                if (_motiveSelectionViewModel.SelectedMotiveOption != null)
+                {
+                    _motiveSelectionViewModel.SelectedMotiveOption.Selected = false;
+                    _motiveSelectionViewModel.CustomMotive = false;
+                }
+
+                _navigation.PushAsync(new SearchMotivePage(_motiveSelectionViewModel));
+                _motiveSelectionViewModel.SelectedMotiveOption = null;
+
+            }
+            else if (motive == CustomMotiveOption)
+            {
+                _navigation.PushAsync(new SearchMotivePage(_motiveSelectionViewModel));
+            }
+            else if (_motiveSelectionViewModel.SelectedMotiveOption != motive)
+            {
+                _motiveSelectionViewModel.ShouldSave = true;
+                _motiveSelectionViewModel.CustomMotive = false;
+
+                if (_motiveSelectionViewModel.SelectedMotiveOption != null)
+                {
+                    // Set previously selected motive to false
+                    _motiveSelectionViewModel.SelectedMotiveOption.Selected = false;
+                }
+                motive.Selected = true;
+                _motiveSelectionViewModel.SelectedMotiveOption = motive;
+            }
+            else
+            {
+                // Clicked the same motive twice; reset selected
+                _motiveSelectionViewModel.CustomMotive = false;
+                _motiveSelectionViewModel.SelectedMotiveOption.Selected = false;
+                _motiveSelectionViewModel.SelectedMotiveOption = null;
+                _motiveSelectionViewModel.ShouldSave = false;
+            }
+        }
+
+        public void Disappear()
+        {
+            if (!saved)
+            {
+                ResetViewModel();
+            }
+        }
+
+        void _motiveSelectionViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_motiveSelectionViewModel.CustomMotive))
+            {
+                if (_motiveSelectionViewModel.CustomMotive)
+                {
+                    CustomMotiveOption.Text = _motiveSelectionViewModel.SelectedMotiveOption.Text;
+                    CustomMotiveOption.Id = _motiveSelectionViewModel.SelectedMotiveOption.Id;
+                    CustomMotiveOption.Selected = true;
+
+                    PossibleMotives.Add(CustomMotiveOption);
+                }
+                else
+                {
+                    PossibleMotives.Remove(CustomMotiveOption);
+                }
+            }
+        }
+
+    }
+
+}
--- src/Tabi.Shared/ViewModels/StopDetailMotiveViewModel.cs
+++ src/Tabi.Shared/ViewModels/StopDetailMotiveViewModel.cs
@@ -10,159 +10,40 @@ using Xamarin.Forms;
 
 namespace Tabi.Shared.ViewModels
 {
-    public class StopDetailMotiveViewModel : BaseViewModel
+    public class StopDetailMotiveViewModel : DetailMotiveViewModel
     {
-        private readonly IRepoManager _repoManager;
-        private readonly INavigation _navigation;
-        private readonly MotiveSelectionViewModel _motiveSelectionViewModel;
-        private MotiveOptionViewModel CustomMotiveOption = new MotiveOptionViewModel();
 
-        private bool saved;
 
         public StopDetailMotiveViewModel(IRepoManager repoManager,
                                          INavigation navigation,
                                          TabiConfiguration configuration,
                                          StopMotiveViewModel motiveViewModel,
-                                         MotiveSelectionViewModel motiveSelectionViewModel)
+                                         MotiveSelectionViewModel motiveSelectionViewModel) : base(repoManager, navigation, configuration, motiveSelectionViewModel)
         {
-            _repoManager = repoManager ?? throw new ArgumentNullException(nameof(repoManager));
             Motive = motiveViewModel ?? throw new ArgumentNullException(nameof(motiveViewModel));
-            _navigation = navigation ?? throw new ArgumentNullException(nameof(navigation));
-            _motiveSelectionViewModel = motiveSelectionViewModel ?? throw new ArgumentNullException(nameof(motiveSelectionViewModel));
 
-            SaveCommand = new Command(async () =>
-            {
-                if (motiveSelectionViewModel.ShouldSave)
-                {
-                    saved = true;
-                    Motive.Text = motiveSelectionViewModel.SelectedMotiveOption.Id;
-                    Motive newMotive = Motive.SaveViewModelToModel();
-                    repoManager.MotiveRepository.Add(newMotive);
-                }
-                await _navigation.PopModalAsync();
-
-            });
-            CancelCommand = new Command(async () =>
-            {
-                Motive.ResetViewModel();
-                await PopPageAsync();
-            });
-
-            _motiveSelectionViewModel.PropertyChanged += _motiveSelectionViewModel_PropertyChanged;
-
-            PossibleMotives = new ObservableRangeCollection<MotiveOptionViewModel>();
-
-            // Setup possible motives
-            foreach (MotiveOption mo in configuration.Motive.Options)
-            {
-                string translatedText = typeof(AppResources).GetProperty($"{mo.Id}MotiveText")?.GetValue(null) as string;
-                string text = translatedText ?? mo.Text;
-                MotiveOptionViewModel motive = new MotiveOptionViewModel() { Text = text, Id = mo.Id };
-                if (motive.Id == Motive.Text)
-                {
-                    motive.Selected = true;
-                    motiveSelectionViewModel.SelectedMotiveOption = motive;
-                }
-                PossibleMotives.Add(motive);
-            }
-
-            foreach (MotiveOption mo in configuration.Motive.OtherOptions)
-            {
-                string translatedText = typeof(AppResources).GetProperty($"{mo.Id}MotiveText")?.GetValue(null) as string;
-                string text = translatedText ?? mo.Text;
-                if (mo.Id == Motive.Text)
-                {
-                    CustomMotiveOption = new MotiveOptionViewModel() { Text = text, Id = mo.Id };
-                    motiveSelectionViewModel.SelectedMotiveOption = CustomMotiveOption;
-                    CustomMotiveOption.Selected = true;
-                    motiveSelectionViewModel.CustomMotive = true;
-
-                    break;
-                }
-            }
-        }
-
-        ObservableRangeCollection<MotiveOptionViewModel> possibleMotives;
-
-        public ObservableRangeCollection<MotiveOptionViewModel> PossibleMotives
-        {
-            get => possibleMotives;
-            set => SetProperty(ref possibleMotives, value);
+            SetupMotives();
         }
 
         public StopMotiveViewModel Motive { get; private set; }
 
-        private async Task PopPageAsync()
+        protected override void ResetViewModel()
         {
-            await _navigation.PopModalAsync();
+            Motive.ResetViewModel();
         }
 
-        public ICommand SaveCommand { get; set; }
-
-        public ICommand CancelCommand { get; set; }
-
-        public void OptionSelected(MotiveOptionViewModel motive)
+        protected override void SaveViewModel()
         {
-            if (motive.Id == "Other")
+            if (_motiveSelectionViewModel.ShouldSave)
             {
-                if (_motiveSelectionViewModel.SelectedMotiveOption != null)
-                {
-                    _motiveSelectionViewModel.SelectedMotiveOption.Selected = false;
-                    _motiveSelectionViewModel.CustomMotive = false;
-                }
-
-                _navigation.PushAsync(new SearchMotivePage(_motiveSelectionViewModel));
-                _motiveSelectionViewModel.SelectedMotiveOption = null;
-
-            }
-            else if (_motiveSelectionViewModel.SelectedMotiveOption != motive)
-            {
-                _motiveSelectionViewModel.ShouldSave = true;
-                _motiveSelectionViewModel.CustomMotive = false;
-
-                if (_motiveSelectionViewModel.SelectedMotiveOption != null)
-                {
-                    // Set previously selected motive to false
-                    _motiveSelectionViewModel.SelectedMotiveOption.Selected = false;
-                }
-                motive.Selected = true;
-                _motiveSelectionViewModel.SelectedMotiveOption = motive;
-            }
-            else
-            {
-                // Clicked the same motive twice; reset selected
-                _motiveSelectionViewModel.CustomMotive = false;
-                _motiveSelectionViewModel.SelectedMotiveOption.Selected = false;
-                _motiveSelectionViewModel.SelectedMotiveOption = null;
-                _motiveSelectionViewModel.ShouldSave = false;
+                saved = true;
+                Motive.Text = _motiveSelectionViewModel.SelectedMotiveOption.Id;
+                Motive newMotive = Motive.SaveViewModelToModel();
+                _repoManager.MotiveRepository.Add(newMotive);
             }
         }
 
-        public void Disappear()
-        {
-            if (!saved)
-            {
-                Motive.ResetViewModel();
-            }
-        }
+        protected override string MotiveText => Motive?.Text;
 
-        void _motiveSelectionViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(_motiveSelectionViewModel.CustomMotive))
-            {
-                if (_motiveSelectionViewModel.CustomMotive)
-                {
-                    CustomMotiveOption.Text = _motiveSelectionViewModel.SelectedMotiveOption.Text;
-                    CustomMotiveOption.Id = _motiveSelectionViewModel.SelectedMotiveOption.Id;
-                    CustomMotiveOption.Selected = true;
-
-                    PossibleMotives.Add(CustomMotiveOption);
-                }
-                else
-                {
-                    PossibleMotives.Remove(CustomMotiveOption);
-                }
-            }
-        }
     }
 }
